1cd18b8ce5a029f36c10467f97b486ffb03c1feb
schmutov <chmutovsergey@gmail.com>
2019-12-14 20:18:45 +0300

    Добавить админку к сервису

--- urlshortener/shortner_app/admin.py
+++ urlshortener/shortner_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Urls
+
+
+@admin.register(Urls)
+class UrlsAdmin(admin.ModelAdmin):
+    list_display = ('md_url', 'short_url', 'http_url', 'pub_date', )
+    ordering = ('-pub_date', )
