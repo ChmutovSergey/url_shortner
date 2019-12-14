fc88c63863e667828f4a4a6f3294d8a1368fbbb1
schmutov <chmutovsergey@gmail.com>
2019-12-14 20:19:13 +0300

    Добавить модель <Urls> к сервису

--- urlshortener/shortner_app/models.py
+++ urlshortener/shortner_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Urls(models.Model):
+
+    md_url = models.CharField(max_length=32, primary_key=True)
+    short_url = models.SlugField(max_length=6)
+    http_url = models.URLField(max_length=500)
+    pub_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.http_url
